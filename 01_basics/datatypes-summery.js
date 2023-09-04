3b0aee2809a9367dab9b0198a0ad0e51a58d906c
Adnanshihabkhan <114224682+Adnanshihabkhan@users.noreply.github.com>
2023-09-04 18:53:55 +0000

    datatypes

--- 01_basics/datatypes-summery.js
+++ 01_basics/datatypes-summery.js
@@ -3,9 +3,37 @@
 //type : String, Number,Boolean,Null, Underined, Symbol, Bigint
 
 const score =  100
-cons
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol("123")
 
+console.log(id === anotherId)
+
+const bigNumber = 13122242424242424242n
 
 //reference (non primitive)
 
  //array, Objects,functions
+
+ //arrray
+ const heros=['shaktiman','naagraj','captain america']
+
+
+ //object
+
+ let myObject={
+    name:"adnan",
+    age : 22
+
+ }
+
+ //Function
+
+ const myFunction = function(){
+    console.log(("helloworld"));
+
+ }
+
+ console.log(myFunction);
